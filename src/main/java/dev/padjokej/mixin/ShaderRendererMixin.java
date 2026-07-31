4d8c7d1de89a344ec2627bcc10448a40e8d4b12d
PadjokeJ <jonatan@padjokej.dev>
2026-07-31 21:57:20 +0200

    feat: put less strain in resource getter

--- src/main/java/dev/padjokej/mixin/ShaderRendererMixin.java
+++ src/main/java/dev/padjokej/mixin/ShaderRendererMixin.java
@@ -4,8 +4,6 @@ import com.mojang.blaze3d.resource.CrossFrameResourcePool;
 import dev.padjokej.registry.ShaderRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelTargetBundle;
-import net.minecraft.client.renderer.PostChain;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -36,10 +34,8 @@ public class ShaderRendererMixin {
     private void renderShader(CallbackInfo ci) {
         ShaderRegistry.forEach(shader -> {
             if (shader.condition().get()) {
-                PostChain pc = minecraft.getShaderManager()
-                        .getPostChain(shader.shaderId(), LevelTargetBundle.MAIN_TARGETS);
-                if (pc != null)
-                    pc.process(minecraft.getMainRenderTarget(), resourcePool);
+                if (shader.postChain() != null)
+                    shader.postChain().process(minecraft.getMainRenderTarget(), resourcePool);
             }
         });
     }
